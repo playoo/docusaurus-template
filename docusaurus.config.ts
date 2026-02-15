da7a5e5ac73de05dd7a2e564fcc824f3ed6eab84
playoo <98441484+playoo@users.noreply.github.com>
2026-02-15 10:07:52 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,6 +57,41 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    algolia: {
+      // Algolia 提供的应用 ID
+      appId: 'YOUR_APP_ID',
+
+      //  公开 API 密钥：提交它没有危险
+      apiKey: 'YOUR_SEARCH_API_KEY',
+
+      indexName: 'YOUR_INDEX_NAME',
+
+      // 可选：见下文
+      contextualSearch: true,
+
+      // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // 可选：替换 Algolia 的部分网址。 在使用相同搜索索引支持多个不同 baseUrl 的部署时非常有用。 你可以在 “from” 中使用正则表达式或字符串。 例如，localhost:3000 和 myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // 或者使用正则表达式：/\/docs\//
+        to: '/',
+      },
+
+      // 可选：Algolia 搜索参数
+      searchParameters: {},
+
+      // 可选：默认启用的搜索页的路径（传递 `false` 以禁用它）
+      searchPagePath: 'search',
+
+      // 可选：是否启用Docsearch 的 insights 功能（默认为 `false`）
+      insights: false,
+
+      // 可选：你是否想使用新的询问 AI特性（默认为未定义）
+      askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
+
+      //... 其他 Algolia 参数
+    },
     navbar: {
       title: '我的收藏夹',
       logo: {
