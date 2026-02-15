eb0fb5cb65d2779482d1cddb097832827ec49b5c
playoo <98441484+playoo@users.noreply.github.com>
2026-02-15 14:41:27 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
       // 可选：替换 Algolia 的部分网址。 在使用相同搜索索引支持多个不同 baseUrl 的部署时非常有用。 你可以在 “from” 中使用正则表达式或字符串。 例如，localhost:3000 和 myCompany.com/docs
       replaceSearchResultPathname: {
         from: '/docs/', // 或者使用正则表达式：/\/docs\//
-        to: '/',
+        to: '/docs/',
       },
 
       // 可选：Algolia 搜索参数
