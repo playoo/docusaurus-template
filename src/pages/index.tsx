3e64c5e1549829df88cee060ba1c55f0e3badb9f
play000 <ldd166@qq.com>
2026-02-16 08:00:42 +0800

    00

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            欢迎进入我的收藏夹！⏱️
           </Link>
         </div>
       </div>
