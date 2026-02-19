285fa72afbff555fdadb2c998f7fa9d8743c4688
playoo <98441484+playoo@users.noreply.github.com>
2026-02-19 11:44:16 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            欢迎进入我的收藏夹！⏱️
+            欢迎进入我的收藏夹！🎉
           </Link>
         </div>
       </div>
